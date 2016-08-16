2122eeaf874d11ec9fb9b8345ce374c1fa98270c
Dante Yu <dante3@126.com>
2016-08-16 19:43:01 +0800

    update sort

--- sort.py
+++ sort.py
@@ -8,7 +8,7 @@ Created on Tue Aug 16 12:16:40 2016
 def SimpleSort(array):
     for i in range(len(array)):
         min = i
-        for j in range(i+1,len(array)):
+        for j in range(i+1, len(array)):
             if array[j] < array[min]:
                 min = j
         if min != i:
@@ -19,7 +19,7 @@ def BubbleSort(array):
     hasSwitched = True
     for i in range(len(array)):
         hasSwitched = False
-        for j in range(1,len(array)-i):
+        for j in range(1, len(array)-i):
             if array[j-1] > array[j]:
                 array[j-1], array[j] = array[j], array[j-1]
                 hasSwitched = True
@@ -31,6 +31,33 @@ def InsertSort(array):
         while j >= 1 and array[j] < array[j-1]:
             array[j], array[j-1] = array[j-1], array[j]
             j -= 1
+
+
+def HeapSort(array):
+    l = len(array)
+    for i in range(1, l/2+1):
+        _heapAdjust(array, l/2-i, l)
+    for i in range(1, l-1):
+        array[l-i], array[0] = array[0], array[l-i]
+        _heapAdjust(array, 0, l-i-1)
+
+
+def _heapAdjust(array, currentRoot, currentRegion):
+    t = array[currentRoot]
+    child = (currentRoot+1)*2-1
+    while child < currentRegion:
+        #找孩子中较大的一个
+        if child < currentRegion-1:
+            if array[child] < array[child+1]:
+                child += 1
+        #如果根已经比孩子们都大，跳过
+        if t >= array[child]:
+            break
+        array[currentRoot] = array[child]
+        #将更改过的子树作为下一循环的处理对象
+        currentRoot = child
+        child *= 2
+    array[currentRoot] = t
 
 
 def QuickSort(array):
@@ -58,3 +85,6 @@ def _quickSort(array, low, high):
         _quickSort(array, i+1, high)
 
 
+s=[4,3,1,6]
+HeapSort(s)
+print s
